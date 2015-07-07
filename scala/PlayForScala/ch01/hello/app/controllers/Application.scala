faea7210370e058fe58e0bedbb5f3ec23d893ac7
luhtonen <luhtonen@gmail.com>
2015-07-07 18:25:03 +0200

    Adding hello method

--- scala/PlayForScala/ch01/hello/app/controllers/Application.scala
+++ scala/PlayForScala/ch01/hello/app/controllers/Application.scala
@@ -1,12 +1,14 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Hello world"))
   }
 
+  def hello(name: String) = Action {
+    Ok(views.html.index("Hello, " + name))
+  }
 }
