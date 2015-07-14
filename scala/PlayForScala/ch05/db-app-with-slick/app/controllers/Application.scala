e3ab4a676431653a7bcce609d3fae9faf671c782
luhtonen <luhtonen@gmail.com>
2015-07-14 20:17:27 +0200

    Sample view to test datastore

--- scala/PlayForScala/ch05/db-app-with-slick/app/controllers/Application.scala
+++ scala/PlayForScala/ch05/db-app-with-slick/app/controllers/Application.scala
@@ -3,12 +3,35 @@ package controllers
 import javax.inject.Inject
 
 import dao.ProductDAO
+import models.Product
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.mvc._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class Application @Inject()(productDAO: ProductDAO) extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = Action.async { implicit request =>
+    productDAO.all.map {
+      case products => Ok(views.html.index(products))
+    }
   }
 
+  val productForm = Form(
+    mapping(
+      "id" -> optional(longNumber),
+      "ean" -> longNumber,
+      "name" -> nonEmptyText,
+      "description" -> optional(text)
+    )(Product.apply)(Product.unapply)
+  )
+
+  def insert = Action.async { implicit request =>
+    val product = productForm.bindFromRequest.get
+    productDAO.insert(product).map(_ => Redirect(routes.Application.index))
+  }
+
+  def reset = Action { implicit request =>
+    Redirect(routes.Application.index)
+  }
 }
