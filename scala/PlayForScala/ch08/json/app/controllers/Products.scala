d4317085fe7131433cb8ad42aad83df807b20a08
luhtonen <luhtonen@gmail.com>
2015-07-16 19:59:06 +0200

    Getting product details using JSON and Writes[T]

--- scala/PlayForScala/ch08/json/app/controllers/Products.scala
+++ scala/PlayForScala/ch08/json/app/controllers/Products.scala
@@ -1,7 +1,8 @@
 package controllers
 
 import models.Product
-import play.api.libs.json.Json
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 import play.api.mvc.{Action, Controller}
 
 /** Created by luhtonen on 16/07/15. */
@@ -11,5 +12,17 @@ class Products extends Controller {
     val productCodes = Product.findAll.map(_.ean)
 
     Ok(Json.toJson(productCodes))
+  }
+
+  implicit val productWrites: Writes[Product] = (
+      (JsPath \ "ean").write[Long] and
+      (JsPath \ "name").write[String] and
+      (JsPath \ "description").write[String]
+    )(unlift(Product.unapply))
+
+  def details(ean: Long) = Action {
+    Product.findByEan(ean).map { product =>
+      Ok(Json.toJson(product))
+    }.getOrElse(NotFound)
   }
 }
