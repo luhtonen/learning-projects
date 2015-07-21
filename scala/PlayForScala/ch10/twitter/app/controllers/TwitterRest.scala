2f253bb6e64882c4d756d4f85c7ae771f8b56aeb
luhtonen <luhtonen@gmail.com>
2015-07-21 08:56:15 +0200

    Implementing non-blocking request

--- scala/PlayForScala/ch10/twitter/app/controllers/TwitterRest.scala
+++ scala/PlayForScala/ch10/twitter/app/controllers/TwitterRest.scala
@@ -3,13 +3,14 @@ package controllers
 import javax.inject.Inject
 
 import models.Tweet
+import play.api.libs.json.Json
 import play.api.libs.ws.WSClient
 import play.api.mvc._
-import play.api.libs.json.Json
 import utils.Twitter
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /** Created by 805515 on 20.07.2015. */
 class TwitterRest @Inject() (ws: WSClient, twitter: Twitter) extends Controller {
@@ -26,5 +27,20 @@ class TwitterRest @Inject() (ws: WSClient, twitter: Twitter) extends Controller 
     val response = Await.result(responseFuture, 10 seconds)
     val tweets = (Json.parse(response.body) \ "statuses").as[Seq[Tweet]]
     Ok(views.html.twitterrest.tweetlist("Blocking tweets", tweets))
+  }
+
+  def nonblocking = Action.async { implicit request =>
+    val count = 3
+    val query = """paperclip OR "paper clip""""
+    twitter.bearerToken.flatMap { bearerToken =>
+      val responseFuture = ws.url("https://api.twitter.com/1.1/search/tweets.json")
+        .withQueryString("q" -> query, "count" -> count.toString)
+        .withHeaders("Authorization" -> s"Bearer $bearerToken")
+        .get()
+      responseFuture.map { response =>
+        val tweets = Json.parse(response.body).\("statuses").as[Seq[Tweet]]
+        Ok(views.html.twitterrest.tweetlist("Non-blocking tweets", tweets))
+      }
+    }
   }
 }
