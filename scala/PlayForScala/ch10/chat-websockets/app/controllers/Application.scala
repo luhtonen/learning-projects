31e00fa1aeccd400856c01f96890027e158a4d10
Eduard Luhtonen <eduard.luhtonen@ch.kuoni.com>
2015-07-22 12:31:00 +0200

    Making joining work and fixing styles

--- scala/PlayForScala/ch10/chat-websockets/app/controllers/Application.scala
+++ scala/PlayForScala/ch10/chat-websockets/app/controllers/Application.scala
@@ -1,11 +1,19 @@
 package controllers
 
 import play.api.mvc._
+import play.api.routing.JavaScriptReverseRouter
 
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Join Chat"))
+    Ok(views.html.index("Chat"))
   }
 
+  def jsRoutes = Action { implicit request =>
+    Ok(
+      JavaScriptReverseRouter("jsRoutes")(
+        routes.javascript.Chat.showRoom
+      )
+    ).as("text/javascript")
+  }
 }
