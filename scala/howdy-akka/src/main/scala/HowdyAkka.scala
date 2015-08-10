c074b7b5b33f3cd2074f37ad872d2265cdaa4eda
luhtonen <luhtonen@gmail.com>
2015-08-10 13:48:11 +0200

    First actor performance

--- scala/howdy-akka/src/main/scala/HowdyAkka.scala
+++ scala/howdy-akka/src/main/scala/HowdyAkka.scala
@@ -1,4 +1,20 @@
+import akka.actor.{Props, ActorSystem, ActorLogging, Actor}
+
 /** Created by luhtonen on 10/08/15. */
+case object Pint
+
+class Person extends Actor with ActorLogging {
+  def receive = {
+    case Pint => log.info("Thanks for the pint")
+  }
+}
+
 object HowdyAkka extends App {
-  println("Howdy Akka")
+  val system = ActorSystem("howdy-akka")
+
+  val alice = system.actorOf(Props(new Person()), "alice")
+
+  alice ! Pint
+
+  system.shutdown()
 }
