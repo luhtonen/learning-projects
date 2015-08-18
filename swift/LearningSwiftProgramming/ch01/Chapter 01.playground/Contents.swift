c4708fb17c48e10b956ab771af85ce6c7686de91
luhtonen <luhtonen@gmail.com>
2015-08-18 10:11:43 +0200

    Learning Swift Programming books first playground example

---
+++ swift/LearningSwiftProgramming/ch01/Chapter 01.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var hasSomething: String? = "Hey there!"
+if let message = hasSomething {
+    "Message was legit: \(message)"
+} else {
+    "There was no message!"
+}
