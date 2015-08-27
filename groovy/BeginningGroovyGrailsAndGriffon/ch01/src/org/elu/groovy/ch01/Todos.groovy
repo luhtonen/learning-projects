5128ec688823fa91848af00bf34977af7740f96e
luhtonen <luhtonen@gmail.com>
2015-08-27 19:59:31 +0200

    Creating Groovy script

---
+++ groovy/BeginningGroovyGrailsAndGriffon/ch01/src/org/elu/groovy/ch01/Todos.groovy
@@ -0,0 +1,16 @@
+package org.elu.groovy.ch01;
+
+public class Todo {
+    String name
+    String note
+}
+
+def todos = [
+        new Todo(name: "1", note: "one"),
+        new Todo(name: "2", note: "two"),
+        new Todo(name: "3", note: "three")
+]
+
+todos.each {
+    println "${it.name} ${it.note}"
+}
