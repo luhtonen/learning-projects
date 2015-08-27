a0fa652dda2c05d15cf41daccacbb591e7b5bb06
luhtonen <luhtonen@gmail.com>
2015-08-27 19:51:16 +0200

    Switching to use Groovy syntax sugar

--- groovy/BeginningGroovyGrailsAndGriffon/ch01/src/org/elu/groovy/ch01/Todo.groovy
+++ groovy/BeginningGroovyGrailsAndGriffon/ch01/src/org/elu/groovy/ch01/Todo.groovy
@@ -1,20 +1,17 @@
 package org.elu.groovy.ch01;
 
-import java.util.List;
-import java.util.ArrayList;
-
 public class Todo {
-    String name;
-    String note;
+    String name
+    String note
 
     public static void main(String[] args) {
-        List<Todo> todos = new ArrayList<>();
-        todos.add(new Todo(name: "1", note: "one"));
-        todos.add(new Todo(name: "2", note: "two"));
-        todos.add(new Todo(name: "3", note: "three"));
+        def todos = new ArrayList()
+        todos.add(new Todo(name: "1", note: "one"))
+        todos.add(new Todo(name: "2", note: "two"))
+        todos.add(new Todo(name: "3", note: "three"))
 
         for (Todo todo: todos) {
-            System.out.println(todo.name + " " + todo.note);
+            println "${todo.name} ${todo.note}"
         }
     }
 }
