31f2ef1d2a7ab23d9379f6e09f1c191b65a382b1
luhtonen <luhtonen@gmail.com>
2015-08-30 12:15:29 +0200

    common uses of regular expressions in Groovy

--- groovy/BeginningGroovyGrailsAndGriffon/ch02/src/regexp.groovy
+++ groovy/BeginningGroovyGrailsAndGriffon/ch02/src/regexp.groovy
@@ -27,3 +27,28 @@ println "Matches = ${count}"
 for (i in 0..<count) {
     println matcher[i]
 }
+
+// uses of regular expressions
+def phoneValidation = /^[01]?\s*[\(\.-]?(\d{3})[\)\.-]?\s*(\d{3})[\.-](\d{4})$/
+assert '(800)555-1212' ==~ phoneValidation
+assert '1(800) 555-1212' ==~ phoneValidation
+assert '1-800-555-1212' ==~ phoneValidation
+assert '1.800.555.1212' ==~ phoneValidation
+
+class Phone {
+    String areaCode
+    String exchange
+    String local
+}
+def phoneStr = '(800)555-1212'
+def phonePattern = ~/^[01]?\s*[\(\.-]?(\d{3})[\)\.-]?\s*(\d{3})[\.-](\d{4})$/
+def phoneMatcher = phonePattern.matcher(phoneStr)
+def phone = new Phone(
+        areaCode: phoneMatcher[0][1],
+        exchange: phoneMatcher[0][2],
+        local: phoneMatcher[0][3])
+println "Original Phone Number: $phoneStr"
+println """Parsed Phone Number\
+\n\tArea Code = ${phone.areaCode}\
+\n\tExchange = ${phone.exchange}\
+\n\tLocal = ${phone.local}"""
