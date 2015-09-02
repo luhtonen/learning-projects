af0ae9e76246a48447c3aaf2a3fae9501e821f1f
luhtonen <luhtonen@gmail.com>
2015-09-02 08:58:51 +0200

    Exercises for Chapter 03 - final version

--- JavaScript/EloquentJavaScript/ch04/exercises.js
+++ JavaScript/EloquentJavaScript/ch04/exercises.js
@@ -19,3 +19,47 @@ function motherChild() {
   });
   console.log(average(diffs));
 }
+
+function groupBy(array, group) {
+  var groups = {};
+  array.forEach(function(person) {
+    var groupName = group(person);
+    if (groupName in groups) {
+      groups[groupName].push(person);
+    } else {
+      groups[groupName] = [person];
+    }
+  });
+  return groups;
+}
+
+function calculateLifeExpectancy() {
+  var centuries = groupBy(ancestry, function(person) {
+    return Math.ceil(person.died / 100);
+  });
+
+  for (var century in centuries) {
+    var ages = centuries[century].map(function(person) {
+      return person.died - person.born;
+    });
+    console.log(century + ": " + average(ages).toFixed(1));
+  }
+}
+
+function every(array, predicate) {
+  for (var i = 0; i < array.length; i++) {
+    if(!predicate(array[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function some(array, predicate) {
+  for (var i = 0; i < array.length; i++) {
+    if(predicate(array[i])) {
+      return true;
+    }
+  }
+  return false;
+}
