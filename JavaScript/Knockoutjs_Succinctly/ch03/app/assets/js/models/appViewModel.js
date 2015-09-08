33294462c2efec7e38a1802d5881abb6310b6f8d
luhtonen <luhtonen@gmail.com>
2015-09-08 16:14:53 +0200

    Chapter 03 project initial version

---
+++ JavaScript/Knockoutjs_Succinctly/ch03/app/assets/js/models/appViewModel.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/* global define:true*/
+define(['jquery',
+    'knockout'
+    ], function ($, ko) {
+  return function () {
+    var self = this;
+
+    self.firstName = ko.observable('John');
+    self.lastName = ko.observable('Smith');
+    self.modalTitle = ko.observable('');
+    self.modalText = ko.observable('');
+    self.checkout = function () {
+      self.modalTitle('Checkout');
+      self.modalText('Trying to checkout');
+    };
+    self.fullName = ko.computed(function () {
+      return self.firstName() + ' ' + self.lastName();
+    }, self);
+  };
+});
