d9bdf9b639dcc8e08086f617fa3ccfbd70ff8aae
luhtonen <luhtonen@gmail.com>
2015-09-10 09:22:27 +0200

    Fixing bug by passing custom parameters

--- JavaScript/Knockoutjs_Succinctly/ch06/app/assets/js/models/appViewModel.js
+++ JavaScript/Knockoutjs_Succinctly/ch06/app/assets/js/models/appViewModel.js
@@ -24,12 +24,12 @@ define(['jquery',
       self.firstName('Bob');
     };
 
-    self.details = ko.observable(false);
-    self.showDetails = function (target, event) {
-      self.details(true);
+    self.details = ko.observable('');
+    self.showDetails = function (target, event, details) {
+      self.details(details);
     };
     self.hideDetails = function (target, event) {
-      self.details(false);
+      self.details('');
     };
   };
 });
