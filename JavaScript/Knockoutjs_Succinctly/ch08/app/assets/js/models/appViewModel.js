fc5a89fd39b784308fcac45d166e5523f23c4853
luhtonen <luhtonen@gmail.com>
2015-09-12 14:25:15 +0200

    adding product table

--- JavaScript/Knockoutjs_Succinctly/ch08/app/assets/js/models/appViewModel.js
+++ JavaScript/Knockoutjs_Succinctly/ch08/app/assets/js/models/appViewModel.js
@@ -7,10 +7,26 @@ define(['jquery',
   return function () {
     var self = this;
 
+    function Product(name, price /*, tags, discount, details */) {
+      this.name = name;
+      this.price = price;
+    }
 
-    // Example observable
-    self.status = ko.observable('active');
+    self.instructions = ko.observable('');
+    self.hasInstructions = ko.observable(false);
 
+    self.items = ko.observableArray([
+      new Product('Beer', 10.99),
+      new Product('Brats', 7.99),
+      new Product('Buns', 1.49)
+    ]);
 
+    self.addProduct = function () {
+      self.items.push(new Product('More Beer', 10.99));
+    };
+
+    self.removeProduct = function (product) {
+      self.items.destroy(product);
+    };
   };
 });
