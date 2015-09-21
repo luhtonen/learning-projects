d607ebcc3445d45f0238a98cefe652e5328cf621
luhtonen <luhtonen@gmail.com>
2015-09-21 20:36:03 +0200

    Data binding using mapping plugin

--- JavaScript/Knockoutjs_Succinctly/knockout-mapping/app/js/main.js
+++ JavaScript/Knockoutjs_Succinctly/knockout-mapping/app/js/main.js
@@ -1,28 +1,19 @@
 (function () {
   'use strict';
 
-  function PersonViewModel () {
-    var self = this;
-    self.firstName = ko.observable('');
-    self.lastName = ko.observable('');
-    self.activities = ko.observable([]);
-    self.favoriteHobby = ko.observable('');
-
-    self.loadUserData = function () {
+  $.getJSON('/get-user-data', function (data) {
+    var viewModel = ko.mapping.fromJS(data);
+    viewModel.loadUserData = function () {
       $.getJSON('/get-user-data', function (data) {
-        self.firstName(data.firstName);
-        self.lastName(data.lastName);
-        self.activities(data.activities);
-        self.favoriteHobby(data.favoriteHobby);
+        ko.mapping.fromJS(data, viewModel);
       });
     };
-
-    self.saveUserData = function () {
-      var data_to_send = {userData: ko.toJSON(self)};
+    viewModel.saveUserData = function () {
+      var data_to_send = {userData: ko.toJSON(viewModel)};
       $.post('/save-user-data', data_to_send, function (data) {
         alert('Your data has been posted to the server!');
       });
     };
-  }
-  ko.applyBindings(new PersonViewModel());
+    ko.applyBindings(viewModel);
+  });
 }());
