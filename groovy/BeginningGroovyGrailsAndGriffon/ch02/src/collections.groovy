b13f5b8793af90f020b700d878a2bb94b1f8d0b6
luhtonen <luhtonen@gmail.com>
2015-09-22 18:51:54 +0200

    Lists in Groovy

--- groovy/BeginningGroovyGrailsAndGriffon/ch02/src/collections.groovy
+++ groovy/BeginningGroovyGrailsAndGriffon/ch02/src/collections.groovy
@@ -10,3 +10,31 @@ println stringArray[1]
 stringArray.each { println it }
 println stringArray[-1..-3]
 
+// Lists
+def emptyList = []
+println emptyList.class.name
+println emptyList.size()
+
+def list = ["Arno"]
+list.add "Lauri"
+list << "Kristian"
+println list.size()
+
+list.each { println it }
+
+println list[1]
+list[0] = "Karoliina"
+println list.get(0)
+
+list.set(0, "Arno")
+println list.get(0)
+
+list.remove 2
+list -= "Lauri"
+list.each { println it }
+
+list.add "Lauri"
+list += "Kristian"
+list.each { println it }
+println list[-1]
+
