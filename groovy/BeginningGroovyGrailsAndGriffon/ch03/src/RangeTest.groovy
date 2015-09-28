601b5f9590926bf8e0dd4d0359b6da8d38dca4ab
luhtonen <luhtonen@gmail.com>
2015-09-28 19:14:29 +0200

    Unit Testing in Groovy

---
+++ groovy/BeginningGroovyGrailsAndGriffon/ch03/src/RangeTest.groovy
@@ -0,0 +1,24 @@
+// Groovy Unit Testing
+class RangeTest extends GroovyTestCase {
+    def lowerCaseRange = 'a'..'z'
+    def upperCaseRange = 'A'..'Z'
+
+    void testLowerCaseRange() {
+        assert 26 == lowerCaseRange.size()
+        assertTrue(lowerCaseRange.contains('b'))
+        assertFalse(lowerCaseRange.contains('B'))
+    }
+
+    void testUpperCaseRange() {
+        assert 26 == upperCaseRange.size()
+        assertTrue(upperCaseRange.contains('B'))
+        assertFalse(upperCaseRange.contains('b'))
+    }
+
+    void testAlphaRange() {
+        def alphaRange = lowerCaseRange + upperCaseRange
+        assert 52 == alphaRange.size()
+        assertTrue(alphaRange.contains('b'))
+        assertTrue(alphaRange.contains('B'))
+    }
+}
