3639cdb2bf7e40f31386fe22df5dfe4e07b5d0ee
luhtonen <luhtonen@gmail.com>
2015-09-29 18:11:16 +0200

    Using methodMissing() in Groovy

---
+++ groovy/BeginningGroovyGrailsAndGriffon/ch03/src/missing.groovy
@@ -0,0 +1,9 @@
+// Using methodMissing()
+class Missing {
+    def methodMissing(String name, args) {
+        "$name method does not exist"
+    }
+}
+Missing m = new Missing()
+assert m.test() == "test method does not exist"
+println m.test()
