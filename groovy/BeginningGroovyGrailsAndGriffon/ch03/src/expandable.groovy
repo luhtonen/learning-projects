656b001aa8bb1a532fc80f6594407e8f05694878
luhtonen <luhtonen@gmail.com>
2015-09-29 18:08:06 +0200

    Overriding getProperty and setProperty in Groovy

---
+++ groovy/BeginningGroovyGrailsAndGriffon/ch03/src/expandable.groovy
@@ -0,0 +1,9 @@
+// Overriding getProperty and setProperty
+class Expandable {
+    def storage = [:]
+    def getProperty(String name) { storage[name] }
+    void setProperty(String name, value) { storage[name] = value }
+}
+def e = new Expandable()
+e.foo = "bar"
+println e.foo
