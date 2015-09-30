f9a1dc1fe4ceec9e464cc04563a97b26f8265b7e
luhtonen <luhtonen@gmail.com>
2015-09-30 23:08:34 +0200

    Grails project initial version

---
+++ groovy/BeginningGroovyGrailsAndGriffon/collab-todo/grails-app/init/collab/todo/Application.groovy
@@ -0,0 +1,10 @@
+package collab.todo
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
