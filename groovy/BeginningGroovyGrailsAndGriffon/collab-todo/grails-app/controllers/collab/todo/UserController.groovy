280ad53cb0facfc349069afe07bbe55dfacfce33
luhtonen <luhtonen@gmail.com>
2015-10-02 19:05:50 +0200

    Regenerating User Controller

--- groovy/BeginningGroovyGrailsAndGriffon/collab-todo/grails-app/controllers/collab/todo/UserController.groovy
+++ groovy/BeginningGroovyGrailsAndGriffon/collab-todo/grails-app/controllers/collab/todo/UserController.groovy
@@ -1,8 +1,26 @@
 package collab.todo
 
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
 class UserController {
 
-    static scaffold = User
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond User.list(params), model:[userCount: User.count()]
+    }
+
+    def show(User user) {
+        respond user
+    }
+
+    def create() {
+        respond new User(params)
+    }
+
     def login() {}
     def handleLogin() {
         def user = User.findByUserName(params.userName)
@@ -20,16 +38,88 @@ class UserController {
             redirect(action: 'login')
         }
     }
-    def save() {
-        def user = new User()
-        user.properties = params
-        if (user.save()) {
-            flash.message = "user.saved.message"
-            flash.args = [user.firstName, user.lastName]
-            flash.defaultMsg = "User Saved"
-            redirect(action: 'show', id: user.id)
-        } else {
-            render(view: 'create', model: [user: user])
+
+    @Transactional
+    def save(User user) {
+        if (user == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (user.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond user.errors, view:'create', model: [user: user]
+            return
+        }
+
+        user.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'user.saved.message', args: [user.firstName, user.lastName])
+                redirect action: 'show', id: user.id
+            }
+            '*' { respond user, [status: CREATED] }
+        }
+    }
+
+    def edit(User user) {
+        respond user
+    }
+
+    @Transactional
+    def update(User user) {
+        if (user == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (user.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond user.errors, view:'edit'
+            return
+        }
+
+        user.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'user.label', default: 'User'), user.id])
+                redirect user
+            }
+            '*'{ respond user, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(User user) {
+
+        if (user == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        user.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'user.label', default: 'User'), user.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
         }
     }
 }
--- groovy/BeginningGroovyGrailsAndGriffon/collab-todo/grails-app/views/user/show.gsp
+++ groovy/BeginningGroovyGrailsAndGriffon/collab-todo/grails-app/views/user/show.gsp
@@ -17,7 +17,7 @@
         <div id="show-user" class="content scaffold-show" role="main">
             <h1><g:message code="default.show.label" args="[entityName]" /></h1>
             <g:if test="${flash.message}">
-            <div class="message" role="status"><g:message code="${flash.message}" args="${flash.args}" default="${flash.defaultMsg}"/></div>
+            <div class="message" role="status">${flash.message}</div>
             </g:if>
             <f:display bean="user" />
             <g:form resource="${this.user}" method="DELETE">
