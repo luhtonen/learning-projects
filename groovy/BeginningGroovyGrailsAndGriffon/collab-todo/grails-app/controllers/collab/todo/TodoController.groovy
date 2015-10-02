d2c40a09acadaf8098f764bb6c205eaad63d0c64
luhtonen <luhtonen@gmail.com>
2015-10-02 18:47:05 +0200

    Simple before and after interceptors added

--- groovy/BeginningGroovyGrailsAndGriffon/collab-todo/grails-app/controllers/collab/todo/TodoController.groovy
+++ groovy/BeginningGroovyGrailsAndGriffon/collab-todo/grails-app/controllers/collab/todo/TodoController.groovy
@@ -3,4 +3,12 @@ package collab.todo
 class TodoController {
 
     static scaffold = Todo
+
+    def beforeInterceptor = {
+        log.trace("${session?.user?.userName} Start action ${controllerName} Controller.${actionName} : parameters $params")
+    }
+
+    def afterInterceptor = { model ->
+        log.trace("${session?.user?.userName} End action ${controllerName} Controller.${actionName} : returns $model")
+    }
 }
