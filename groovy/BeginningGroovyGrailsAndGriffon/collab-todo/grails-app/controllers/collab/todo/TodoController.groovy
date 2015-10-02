c862ec8f94f6744865867d00506b2783b6701258
luhtonen <luhtonen@gmail.com>
2015-10-02 18:50:27 +0200

    Applying interceptors conditionally

--- groovy/BeginningGroovyGrailsAndGriffon/collab-todo/grails-app/controllers/collab/todo/TodoController.groovy
+++ groovy/BeginningGroovyGrailsAndGriffon/collab-todo/grails-app/controllers/collab/todo/TodoController.groovy
@@ -4,11 +4,12 @@ class TodoController {
 
     static scaffold = Todo
 
-    def beforeInterceptor = {
+    def beforeInterceptor = [action: this.&beforeAudit, except: ['list']]
+    def afterInterceptor = [action: {model -> this.&afterAudit(model)}, except: ['list']]
+    def beforeAudit = {
         log.trace("${session?.user?.userName} Start action ${controllerName} Controller.${actionName} : parameters $params")
     }
-
-    def afterInterceptor = { model ->
+    def afterAudit = { model ->
         log.trace("${session?.user?.userName} End action ${controllerName} Controller.${actionName} : returns $model")
     }
 }
