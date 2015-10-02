62e78ee3cca72e389e102c0cc80d77921a6d8cb6
luhtonen <luhtonen@gmail.com>
2015-10-02 12:56:13 +0200

    Implementing login and logout actions

--- groovy/BeginningGroovyGrailsAndGriffon/collab-todo/grails-app/controllers/collab/todo/UserController.groovy
+++ groovy/BeginningGroovyGrailsAndGriffon/collab-todo/grails-app/controllers/collab/todo/UserController.groovy
@@ -3,4 +3,21 @@ package collab.todo
 class UserController {
 
     static scaffold = User
+    def login() {}
+    def handleLogin() {
+        def user = User.findByUserName(params.userName)
+        if (!user) {
+            flash.message = "User not found userName: ${params.userName}"
+            redirect(action: 'login')
+        } else {
+            session.user = user
+            redirect(controller: 'todo')
+        }
+    }
+    def logout() {
+        if (session.user) {
+            session.user = null
+            redirect(action: 'login')
+        }
+    }
 }
