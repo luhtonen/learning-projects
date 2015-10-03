d2540c127b4a712b84640f7580a73d4715fb30db
luhtonen <luhtonen@gmail.com>
2015-10-03 19:11:41 +0200

    switching to static scaffolding

--- groovy/BeginningGroovyGrailsAndGriffon/collab-todo/grails-app/conf/UserFilters.groovy
+++ groovy/BeginningGroovyGrailsAndGriffon/collab-todo/grails-app/conf/UserFilters.groovy
@@ -2,23 +2,16 @@ class UserFilters {
     def filters = {
         userModificationCheck(controller: 'user', action: '*') {
             before = {
-                println "applying before filter for action ..."
                 def currActionName = actionName
-                println "checking action ..."
                 if (currActionName == 'edit' || currActionName == 'update' || currActionName == 'delete') {
-                    println "action checked"
                     String userId = session?.user?.id
                     String paramsUserId = params?.id
-                    println "checking user id ..."
                     if (userId != paramsUserId) {
-                        println "user id check failed, should stop action"
                         flash.message = "You can only modify yourself"
-                        redirect(action: 'list')
+                        redirect(action: 'index')
                         return false
                     }
-                    println "user id check passed"
                 }
-                println "action check passed"
             }
         }
     }
