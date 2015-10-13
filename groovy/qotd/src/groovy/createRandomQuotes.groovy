3609ba6659e4363ae0054f38dd7fa93799270068
luhtonen <luhtonen@gmail.com>
2015-10-13 18:53:01 +0200

    Pig Latinization

--- groovy/qotd/src/groovy/createRandomQuotes.groovy
+++ groovy/qotd/src/groovy/createRandomQuotes.groovy
@@ -3,11 +3,30 @@ def authors = [
         "Peter Ledbrook",
         "Glen Smith"
 ]
+
 def quoteParts = [
         ["Time", "waits", "for no man"],
         ["The roundhouse kick", "solves", "all problems"],
         ["Groovy", "is", "the bees knees"]
 ]
+
+for (int i in 0..<10) {
+    def quote = createQuote(quoteParts, authors)
+    println quote
+}
+
+for (int i in 0..<10) {
+    def quote = createQuote(quoteParts, authors)
+    println quote
+
+    def pigLatinWords = []
+    for (String word in quote.split(/\s+/)) {
+        pigLatinWords << pigLatinize(word)
+    }
+
+    println pigLatinWords.join(' ')
+}
+
 String createQuote(List quoteParts, List authors) {
     def rand = new Random()
     def n = quoteParts.size()
@@ -17,7 +36,23 @@ String createQuote(List quoteParts, List authors) {
             quoteParts[rand.nextInt(n)][2] + ' by ' +
             authors[rand.nextInt(m)]
 }
-for (int i in 0..<10) {
-    def quote = createQuote(quoteParts, authors)
-    println quote
+
+def pigLatinize(String word) {
+    if (isVowel(word[0])) {
+        return word + "way"
+    } else {
+        def pos = firstVowel(word)
+        return word[pos..-1] + word[0..<pos] + "ay"
+    }
+}
+
+def firstVowel(String word) {
+    for (int i in 0..<word.size()) {
+        if (isVowel(word[i])) return i
+    }
+    return -1
+}
+
+def isVowel(String ch) {
+    return ch.toLowerCase() in ["a", "e", "i", "o", "u"]
 }
