c2cb081e65a967ac52652045c1155f994817aeb7
luhtonen <luhtonen@gmail.com>
2015-10-14 07:32:41 +0200

    Parametrizing test input and output

--- groovy/qotd/test/unit/org/elu/grailsinaction/qotd/QuoteAnalyzerSpec.groovy
+++ groovy/qotd/test/unit/org/elu/grailsinaction/qotd/QuoteAnalyzerSpec.groovy
@@ -2,6 +2,7 @@ package org.elu.grailsinaction.qotd
 
 import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
@@ -15,14 +16,20 @@ class QuoteAnalyzerSpec extends Specification {
                     content: "Groovy solves all problems")
     ]
 
+    @Unroll
     void "Total number of quotes"() {
         given: "An analyzer initialized with known quotes"
-        def analyzer = new QuoteAnalyzer(quotes)
+        def analyzer = new QuoteAnalyzer(inputQuotes)
 
         when: "I ask for the quote count"
         def quoteCount = analyzer.quoteCount
 
         then: "The number of quotes in the test list is returned"
-        quoteCount == 2
+        quoteCount == expected
+
+        where:
+        inputQuotes | expected
+        []          | 0
+        quotes      | 2
     }
 }
