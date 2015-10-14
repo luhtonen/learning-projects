da8f28334a3a0c7b49abf8ed21c0d7d2dfed7843
luhtonen <luhtonen@gmail.com>
2015-10-14 07:22:27 +0200

    Creating unit test

---
+++ groovy/qotd/test/unit/org/elu/grailsinaction/qotd/QuoteAnalyzerSpec.groovy
@@ -0,0 +1,28 @@
+package org.elu.grailsinaction.qotd
+
+import spock.lang.Shared
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
+ */
+class QuoteAnalyzerSpec extends Specification {
+
+    @Shared quotes = [
+            new Quote(author: "Peter Ledbrook",
+                    content: "Time waits for no man"),
+            new Quote(author: "Glen Smith",
+                    content: "Groovy solves all problems")
+    ]
+
+    void "Total number of quotes"() {
+        given: "An analyzer initialized with known quotes"
+        def analyzer = new QuoteAnalyzer(quotes)
+
+        when: "I ask for the quote count"
+        def quoteCount = analyzer.quoteCount
+
+        then: "The number of quotes in the test list is returned"
+        quoteCount == 2
+    }
+}
