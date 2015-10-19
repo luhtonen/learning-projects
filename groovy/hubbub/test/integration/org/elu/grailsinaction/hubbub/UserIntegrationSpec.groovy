b1956517a97fb1bf435ef5a2cd96411ee0a8e5b9
luhtonen <luhtonen@gmail.com>
2015-10-19 17:26:35 +0200

    Adding more integration test for User domain class

--- groovy/hubbub/test/integration/org/elu/grailsinaction/hubbub/UserIntegrationSpec.groovy
+++ groovy/hubbub/test/integration/org/elu/grailsinaction/hubbub/UserIntegrationSpec.groovy
@@ -22,4 +22,31 @@ class UserIntegrationSpec extends IntegrationSpec {
         joe.id != null
         User.get(joe.id).loginId == joe.loginId
     }
+
+    def "Updating a saved user changes its properties"() {
+        given: "An existing user"
+        def existingUser = new User(loginId: 'joe', password: 'secret', homepage: 'http://www.grailsinaction.com')
+        existingUser.save(failOnError: true)
+
+        when: "A property is changed"
+        def foundUser = User.get(existingUser.id)
+        foundUser.password = 'sesame'
+        foundUser.save(failOnError: true)
+
+        then: "The change is reflected in the database"
+        User.get(existingUser.id).password == 'sesame'
+    }
+
+    def "Deleting an existing user removes it from the database"() {
+        given: "An existing user"
+        def user = new User(loginId: 'joe', password: 'secret', homepage: 'http://www.grailsinaction.com')
+        user.save(failOnError: true)
+
+        when: "The user is deleted"
+        def foundUser = User.get(user.id)
+        foundUser.delete(flush: true)
+
+        then: "The user is removed from the database"
+        !User.exists(foundUser.id)
+    }
 }
