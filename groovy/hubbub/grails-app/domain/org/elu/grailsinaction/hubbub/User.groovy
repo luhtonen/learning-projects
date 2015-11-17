cf2eba59e98c902df3562d2d1eafad97f5f6d809
luhtonen <luhtonen@gmail.com>
2015-11-17 09:03:19 +0100

    Adding one-to-one relationship

--- groovy/hubbub/grails-app/domain/org/elu/grailsinaction/hubbub/User.groovy
+++ groovy/hubbub/grails-app/domain/org/elu/grailsinaction/hubbub/User.groovy
@@ -4,14 +4,15 @@ class User {
 
     String loginId
     String password
-    String homepage
     Date dateCreated
+
+    static hasOne = [ profile: Profile ]
 
     static constraints = {
         loginId size: 3..20, unique: true, blank: false
         password size: 6..8, blank: false, validator: { passwd, user ->
             passwd != user.loginId
         }
-        homepage url: true, nullable: true
+        profile nullable: true
     }
 }
