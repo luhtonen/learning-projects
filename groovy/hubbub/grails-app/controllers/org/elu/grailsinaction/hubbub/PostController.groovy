64790113be3531e34ca9d59c33866dfb0a7f0ce5
luhtonen <luhtonen@gmail.com>
2015-11-30 07:57:09 +0100

    Post timeline implementation

--- groovy/hubbub/grails-app/controllers/org/elu/grailsinaction/hubbub/PostController.groovy
+++ groovy/hubbub/grails-app/controllers/org/elu/grailsinaction/hubbub/PostController.groovy
@@ -2,4 +2,13 @@ package org.elu.grailsinaction.hubbub
 
 class PostController {
     static scaffold = true
+
+    def timeline() {
+        def user = User.findByLoginId(params.id)
+        if (!user) {
+            response.sendError(404)
+        } else {
+            [ user: user ]
+        }
+    }
 }
