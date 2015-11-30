064b636a6c2826108206f0fd31fd86c012708f86
luhtonen <luhtonen@gmail.com>
2015-11-30 08:08:38 +0100

    Creating unit test

--- groovy/hubbub/test/unit/org/elu/grailsinaction/hubbub/PostControllerSpec.groovy
+++ groovy/hubbub/test/unit/org/elu/grailsinaction/hubbub/PostControllerSpec.groovy
@@ -1,5 +1,6 @@
 package org.elu.grailsinaction.hubbub
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,14 +8,24 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(PostController)
+@Mock([User,Post])
 class PostControllerSpec extends Specification {
 
-    def setup() {
-    }
+    void "Get a users timeline given their id"() {
+        given: 'A user with posts in db'
+        User chuck = new User(loginId: 'chuck_norris', password: 'password')
+        chuck.addToPosts(new Post(content: 'A first post'))
+        chuck.addToPosts(new Post(content: 'A second post'))
+        chuck.save(failOnError: true)
 
-    def cleanup() {
-    }
+        and: 'A loginId parameters'
+        params.id = chuck.loginId
 
-    void "test something"() {
+        when: 'A timeline is invoked'
+        def model = controller.timeline()
+
+        then: 'the user is in the returned model'
+        model.user.loginId == 'chuck_norris'
+        model.user.posts.size() == 2
     }
 }
