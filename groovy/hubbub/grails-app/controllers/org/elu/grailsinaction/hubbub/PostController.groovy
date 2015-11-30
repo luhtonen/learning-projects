15f35746c3542d21c5b64873c372e894c81625ff
luhtonen <luhtonen@gmail.com>
2015-11-30 19:46:05 +0100

    implementing form submission

--- groovy/hubbub/grails-app/controllers/org/elu/grailsinaction/hubbub/PostController.groovy
+++ groovy/hubbub/grails-app/controllers/org/elu/grailsinaction/hubbub/PostController.groovy
@@ -11,4 +11,27 @@ class PostController {
             [ user: user ]
         }
     }
+
+    def addPost() {
+        def user = User.findByLoginId(params.id)
+        if (user) {
+            def post = new Post(params)
+            user.addToPosts(post)
+            User.withTransaction {
+                if (user.save()) {
+                    println 'saved ###'
+                    println 'post ' + post + ' with content ' + post?.content
+                    flash.message = 'Successfully created post'
+                } else {
+                    println 'error'
+                    flash.message = 'Invalid or empty post'
+                }
+            }
+        } else {
+            println 'user not found'
+            flash.message = 'Invalid user id'
+        }
+        println 'user ' + user.loginId + ' with posts ' + user.posts
+        redirect(action: 'timeline', id: params.id)
+    }
 }
