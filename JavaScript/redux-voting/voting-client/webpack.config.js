67a2ea649b6c93308b0d10b141fc32e919a07001
luhtonen <luhtonen@gmail.com>
2015-12-27 12:20:37 +0200

    Voting client initial setup

---
+++ JavaScript/redux-voting/voting-client/webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    entry: {
+        javascript: './src/index.js',
+        html: "./src/index.html"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            },
+            {
+                test: /\.html$/,
+                loader: "file?name=[name].[ext]"
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+};
