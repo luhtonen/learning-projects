36cb805dae4badc6233316e0a62146993eb21b14
luhtonen <luhtonen@gmail.com>
2015-12-27 13:38:23 +0200

    Creating voting component

--- JavaScript/redux-voting/voting-client/src/index.jsx
+++ JavaScript/redux-voting/voting-client/src/index.jsx
@@ -1,1 +1,12 @@
-console.log('I am alive');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Voting from './components/Voting';
+
+require('./style.css');
+
+const pair = ['Trainspotting', '28 Days Later'];
+
+ReactDOM.render(
+    <Voting pair={pair} />,
+    document.getElementById('app')
+);
