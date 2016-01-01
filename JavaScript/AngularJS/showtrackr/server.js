fc4910552cd0cfb89cfb9145c39563cb4c4ed646
luhtonen <luhtonen@gmail.com>
2016-01-01 11:38:12 +0200

    Implementing login, signup and subscription on the server side

--- JavaScript/AngularJS/showtrackr/server.js
+++ JavaScript/AngularJS/showtrackr/server.js
@@ -9,6 +9,10 @@ var request = require('request');
 var xml2js = require('xml2js');
 var _ = require('lodash');
 
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
 
@@ -66,14 +70,53 @@ var Show = mongoose.model('Show', showSchema);
 
 mongoose.connect('localhost');
 
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, done) {
+  User.findOne({ email: email }, function(err, user) {
+    if (err) return done(err);
+    if (!user) return done(null, false);
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) return done(err);
+      if (isMatch) return done(null, user);
+      return done(null, false);
+    });
+  });
+}));
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(session({ secret: 'keyboard cat',
+  saveUninitialized: true,
+  resave: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) next();
+  else res.send(401);
+}
+
+app.use(function(req, res, next) {
+  if (req.user) {
+    res.cookie('user', JSON.stringify(req.user));
+  }
+  next();
+});
 
 app.get('/api/shows', function(req, res, next) {
   var query = Show.find();
@@ -175,6 +218,27 @@ app.post('/api/shows', function(req, res, next) {
       res.send(200);
     });
   });
+});
+
+app.post('/api/login', passport.authenticate('local'), function(req, res) {
+  res.cookie('user', JSON.stringify(req.user));
+  res.send(req.user);
+});
+
+app.post('/api/signup', function(req, res, next) {
+  var user = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+  user.save(function(err) {
+    if (err) return next(err);
+    res.sendStatus(200);
+  });
+});
+
+app.get('/api/logout', function(req, res, next) {
+  req.logout();
+  res.sendStatus(200);
 });
 
 app.get('*', function(req, res) {
