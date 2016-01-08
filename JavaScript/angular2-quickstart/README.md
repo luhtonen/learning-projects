88b0e86a2828603baf8c33e1cc2184061a789561
luhtonen <luhtonen@gmail.com>
2016-01-08 10:09:21 +0100

    My first Angular 2 application

---
+++ JavaScript/angular2-quickstart/README.md
@@ -0,0 +1,3 @@
+# angular2-quickstart
+
+First very simple Angular 2 application 
---
+++ JavaScript/angular2-quickstart/app/app.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Main application component.
+ *
+ * Created by luhtonen on 08/01/16.
+ */
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { }
