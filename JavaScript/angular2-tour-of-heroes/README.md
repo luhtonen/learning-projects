d11e66b9bce162083b85732119657e322f2e5344
luhtonen <luhtonen@gmail.com>
2016-01-16 08:42:44 +0100

    Angular 2 Tour of Heroes app - initial version

---
+++ JavaScript/angular2-tour-of-heroes/README.md
@@ -0,0 +1,3 @@
+# angular2-tour-of-heroes
+
+Simple Angular 2 Tour of Heroes application based on [TUTORIAL: TOUR OF HEROES](https://angular.io/docs/ts/latest/tutorial/)
---
+++ JavaScript/angular2-tour-of-heroes/app/boot.ts
@@ -0,0 +1,8 @@
+/**
+ * Boot script.
+ * Created by luhtonen on 08/01/16.
+ */
+import {bootstrap} from 'angular2/platform/browser'
+import {AppComponent} from './app.component'
+
+bootstrap(AppComponent);
