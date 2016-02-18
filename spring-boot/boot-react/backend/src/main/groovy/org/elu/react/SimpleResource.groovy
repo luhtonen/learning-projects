0d1ca4a1e625f4ba38fbaf890e048dc8bd3d9804
luhtonen <luhtonen@gmail.com>
2016-02-18 08:35:37 +0100

    Spring Boot with React.js application initial setup with backend

---
+++ spring-boot/boot-react/backend/src/main/groovy/org/elu/react/SimpleResource.groovy
@@ -0,0 +1,13 @@
+package org.elu.react
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class SimpleResource {
+
+    @RequestMapping('/api/simple')
+    Map resource() {
+        [simple: 'resource']
+    }
+}
