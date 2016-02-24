99a11c12f745672ef4b501075f279f318ebc3d7b
luhtonen <luhtonen@gmail.com>
2016-02-24 16:42:03 +0100

    Adding New Items to the List

--- kanban_app/app/components/App.jsx
+++ kanban_app/app/components/App.jsx
@@ -2,21 +2,30 @@ import uuid from 'node-uuid';
 import React from 'react';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do Laundry'
+        }
+      ]
+    };
+  }
+
   render() {
-    const notes = [
-      {
-        id: uuid.v4(),
-        task: 'Learn Webpack'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Learn React'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Do Laundry'
-      }
-    ];
+    const notes = this.state.notes;
+
     return (
       <div>
         <ul>{notes.map(note =>
